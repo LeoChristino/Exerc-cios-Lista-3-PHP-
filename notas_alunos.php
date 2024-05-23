33b9ddfed8ae803f45a521e8e1dbf79d9c7d5ecc
leochristino <leo.chrissantos28@gmail.com>
2024-05-22 22:12:59 -0300

    primeiro commit

--- notas_alunos.php
+++ notas_alunos.php
@@ -1,6 +1,10 @@
 <?php
-$alunos = array('Lucas' => 5,'Leonardo' => 5,'Matheus' => 3,'Julia'=> 4,'Ana' => 9,'Thiago' => 4,'Rafael' => 2,'Gabriella'=>4,'Maria'=>7,'Bruna'=>9);
-echo "A média das notas é ".array_sum($alunos)/10;
+$alunos = array('Lucas' => 5,'Leonardo' => 5,'Matheus' => 3,'Julia'=> 4,'Ana' => 9,'Thiago' => 4,'Rafael' => 2,'Gabriella'=>4,'Maria'=>7,'Bruna'=>9,'Joao' => 10);
+$cont = 0;
+foreach ($alunos as $key => $value) {
+		$cont++;
+}
+echo "A média das notas é ".array_sum($alunos)/$cont;
 arsort($alunos);
 echo "<br>A maior nota é do/a ";
 echo key($alunos);
